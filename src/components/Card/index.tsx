8af1f843eb96ccdcaa332c05d5c3939457092920
HelioPompei <helioberizonzipompei2017@gmail.com>
2023-08-02 23:20:38 -0300

    Fix: Fixing import error

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,5 @@
 import * as S from "./styles";
 import { projects } from "../../data/projects";
-import { SiGithub } from "react-icons/si";
 
 export const Card = () => {
   return (
@@ -12,7 +11,7 @@ export const Card = () => {
           </S.TitleProject>
 
           <S.ImageContainer src={project.image} alt={project.title} />
-          
+
           <p>{project.description}</p>
         </S.Card>
       ))}
